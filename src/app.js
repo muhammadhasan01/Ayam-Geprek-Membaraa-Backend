a208007cf0b21a51bf305f1fe7a3c6298e406875
primayoriko <primayoriko@gmail.com>
2019-08-10 05:34:33 +0700

    adding comment in app.js part 2

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
-const cors = require('cors')
+const cors = require('cors');
 
 const CONNECTION_URL = "mongodb+srv://spartahmif:decrypt2018@membara-wljwl.mongodb.net/test?retryWrites=true&w=majority";
 const DATABASE_NAME = "example";
 
-// Declaring usage of Express framework
 var app = Express();
 
 app.use(BodyParser.json());
@@ -17,12 +16,13 @@ app.use(cors());
 
 var database, collection;
 
-// Make serven listen to every request that made
+// Make server listen to every request that made
 app.listen(3001, () => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
         if(error) {
             throw error;
         }
+        //connecting to the mongoDB Atlas database if database can be accessed
         database = client.db(DATABASE_NAME);
         collection_acc = database.collection("example");
         collection_ord = database.collection("order");
@@ -34,7 +34,7 @@ app.listen(3001, () => {
 // based on POST request from the HTML registration form
 app.post("/signup", (request, response) => {
 	console.log(request.body);
-    collection_acc.insertOne(request.body, (error, result) => {
+    collection_acc.insertOne(request.body, (error, result) => { // function that add the data inputted to the database
         if(error) {
             return response.status(500).send(error);
         }
@@ -42,11 +42,12 @@ app.post("/signup", (request, response) => {
     });
 });
 
-//Routing to 'web'/login and responding POST request by validating or authenticating
+//Routing to 'web'/login and responding HTTP POST request by validating or authenticating
 // by checking on the database that user input on HTML form
 app.post("/login", (request, response) => {
 	console.log(request.body);
     collection_acc.findOne({ "username": request.body.username, "password": request.body.password }, (error, result) => {
+    // function that searching the inputted data in the database
         if(error) {
             return response.status(500).send(error);
         }
@@ -54,11 +55,12 @@ app.post("/login", (request, response) => {
     });
 });
 
-// Routing to 'web'/order directory and responding POST request  from the HTML form
+// Routing to 'web'/order directory and responding HTTP POST request  from the HTML form
 // by adding the menu that user select to the order list data
 app.post("/order", (request, response) => {
 	console.log(request.body);
     collection_ord.insertOne({...request.body, time: new Date(Date.now()).toISOString()}, (error, result) => {
+    // function that searching the inputted data in the database
         if(error) {
             return response.status(500).send(error);
         }
@@ -66,10 +68,11 @@ app.post("/order", (request, response) => {
     });
 });
 
-// Routing to 'web'/viewOrder and responding GET request by giving the menu list
+// Routing to 'web'/viewOrder and responding HTTP GET request by giving the menu list
 // that user submitted before on 'web'/viewOrder HTML form
 app.get("/viewOrder", (request, response) => {
     collection_ord.find({}).sort({time:1}).toArray((error, result) => {
+    //function that giving all data on specified database
         if(error) {
             return response.status(500).send(error);
         }
@@ -77,11 +80,12 @@ app.get("/viewOrder", (request, response) => {
     });
 });
 
-// Routing 'web'/orderdone and reponding the POST HTML request by
-//
+// Routing 'web'/orderdone and responding the HTTP POST HTML request by
+// adding the order that user made to his order's history
 app.post("/orderdone", (request, response) => {
     console.log(request.body);
     collection_ord.findOneAndUpdate({ "_id": new ObjectId(request.body._id) }, { $set: { done: true } }, (error, result) => {
+    // function that searching the inputted data in the database and then adding new data there
         if(error) {
             return response.status(500).send(error);
         }
