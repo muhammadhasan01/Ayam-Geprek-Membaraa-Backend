d12a83e7dea85bb94c455e906970dcdc332b8b18
primayoriko <primayoriko@gmail.com>
2019-08-10 05:16:08 +0700

    adding comment in app.js part 1

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// Load Modules
 const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
@@ -7,15 +8,16 @@ const cors = require('cors')
 const CONNECTION_URL = "mongodb+srv://spartahmif:decrypt2018@membara-wljwl.mongodb.net/test?retryWrites=true&w=majority";
 const DATABASE_NAME = "example";
 
-
+// Declaring usage of Express framework
 var app = Express();
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
 var database, collection;
 
+// Make serven listen to every request that made
 app.listen(3001, () => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
         if(error) {
@@ -28,8 +30,10 @@ app.listen(3001, () => {
     });
 });
 
+// Routing to  'web'/signup and insert new data on the database
+// based on POST request from the HTML registration form
 app.post("/signup", (request, response) => {
-	console.log(request.body)
+	console.log(request.body);
     collection_acc.insertOne(request.body, (error, result) => {
         if(error) {
             return response.status(500).send(error);
@@ -38,8 +42,10 @@ app.post("/signup", (request, response) => {
     });
 });
 
+//Routing to 'web'/login and responding POST request by validating or authenticating
+// by checking on the database that user input on HTML form
 app.post("/login", (request, response) => {
-	console.log(request.body)
+	console.log(request.body);
     collection_acc.findOne({ "username": request.body.username, "password": request.body.password }, (error, result) => {
         if(error) {
             return response.status(500).send(error);
@@ -48,8 +54,10 @@ app.post("/login", (request, response) => {
     });
 });
 
+// Routing to 'web'/order directory and responding POST request  from the HTML form
+// by adding the menu that user select to the order list data
 app.post("/order", (request, response) => {
-	console.log(request.body)
+	console.log(request.body);
     collection_ord.insertOne({...request.body, time: new Date(Date.now()).toISOString()}, (error, result) => {
         if(error) {
             return response.status(500).send(error);
@@ -58,6 +66,8 @@ app.post("/order", (request, response) => {
     });
 });
 
+// Routing to 'web'/viewOrder and responding GET request by giving the menu list
+// that user submitted before on 'web'/viewOrder HTML form
 app.get("/viewOrder", (request, response) => {
     collection_ord.find({}).sort({time:1}).toArray((error, result) => {
         if(error) {
@@ -67,8 +77,10 @@ app.get("/viewOrder", (request, response) => {
     });
 });
 
+// Routing 'web'/orderdone and reponding the POST HTML request by
+//
 app.post("/orderdone", (request, response) => {
-    console.log(request.body)
+    console.log(request.body);
     collection_ord.findOneAndUpdate({ "_id": new ObjectId(request.body._id) }, { $set: { done: true } }, (error, result) => {
         if(error) {
             return response.status(500).send(error);
